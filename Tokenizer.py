302307789328539a7302ec67029fd500a891a2d3
yoitsmeyusuf <muhammetyusufkaydi@gmail.com>
2025-11-04 19:35:39 +0300

    first commit

---
+++ Tokenizer.py
@@ -0,0 +1,45 @@
+
+import json
+
+# Subword tokenizer 
+class Tokenizer:
+    def __init__(self, vocab_file):
+        self.vocab = self.load_vocab(vocab_file)
+        self.reverse_vocab = {id: word for word, id in self.vocab.items()}
+
+    def load_vocab(self, vocab_file):
+        with open(vocab_file, "r") as f:
+            return json.load(f)
+
+    def encode(self,text):
+        Tokens = []
+        # States
+        # State
+        # s
+        
+        for word in text.split():
+            i = 0
+            while i < len(word):
+                match = None
+                for j in range(len(word), i, -1):
+                    subword = word[i:j]
+                    if subword in self.vocab:
+                        match = subword
+                        break
+                if match:
+                    Tokens.append(self.vocab[match])
+                    i += len(match)
+                else:
+                    Tokens.append(self.vocab.get("<unk>", 0))
+                    i += 1
+            Tokens.append(self.vocab[" "])  # End of word token
+            
+        Tokens.pop()  # Remove the last added end-of-word token    
+        return Tokens
+
+    def decode(self, token_ids):
+        text = ""
+        for token_id in token_ids:
+            text += self.reverse_vocab[token_id] 
+            
+        return text
